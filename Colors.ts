9eb5aee8fd2d416ba84ecb748a1eb9825e409ccd
JaakkoGee <131030750+JageTheProgrammer@users.noreply.github.com>
2025-06-28 21:24:26 +0300

    Create Colors.ts

---
+++ Colors.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    background: '#000000',
+    primary: '#FF6A00',
+    text: '#FFFFFF',
+    secondaryText: '#AAAAAA',
+    accent: '#FF9F40',
+  },
+};
+
+export default theme;
