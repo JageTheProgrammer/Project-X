939685e1409f4579bd80c38fda87af38a141b05b
JaakkoGee <131030750+JageTheProgrammer@users.noreply.github.com>
2025-06-29 21:04:16 +0300

    Update Colors.ts

--- Colors.ts
+++ Colors.ts
@@ -9,3 +9,32 @@ const theme = {
 };
 
 export default theme;
+
+
+
+{
+  "name": "social-chat-app",
+  "version": "1.0.0",
+  "main": "App.js",
+  "scripts": {
+    "start": "expo start",
+    "android": "expo start --android",
+    "ios": "expo start --ios",
+    "web": "expo start --web"
+  },
+  "dependencies": {
+    "expo": "~53.0.0",
+    "react": "18.2.0",
+    "react-native": "0.73.0",
+    "expo-image-picker": "~15.4.1",
+    "axios": "^1.4.0",
+    "@react-native-async-storage/async-storage": "~1.17.11",
+    "@react-navigation/native": "^6.1.6",
+    "@react-navigation/bottom-tabs": "^6.5.7",
+    "@react-navigation/native-stack": "^6.9.12",
+    "react-native-gesture-handler": "~2.14.0",
+    "react-native-safe-area-context": "4.8.2",
+    "react-native-screens": "~3.30.0",
+    "socket.io-client": "^4.7.4"
+  }
+}
